ddb130f0ddadc3ef4be693e0d6ea18305ec21d10
Night <rolyalnight@gmail.com>
2020-08-26 12:18:09 +0700

    first commit

---
+++ server.php
@@ -0,0 +1,21 @@
+<?php
+    //declear db 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "garbage";
+
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "connected";
+    }
+
+
+
+
+
+?>
